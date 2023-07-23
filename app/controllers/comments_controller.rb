55634d8d3255e5a809a77d13051862bdae39956f
Lucas Almeida <lucsa.dev@gmail.com>
2023-07-22 21:21:53 -0300

    feat: create commenta

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,19 @@
 class CommentsController < ApplicationController
-    private
+  http_basic_authenticate_with name: "dhh", password: "secret", only: :destroy
+  
+  def create
+    puts "comment_params: #{comment_params}"
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.create(comment_params)
+  end
+  
+  def destroy
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+    @comment.destroy
+  end
+  
+  private
     def comment_params
       params.require(:comment).permit(:commenter, :body, :status)
     end
